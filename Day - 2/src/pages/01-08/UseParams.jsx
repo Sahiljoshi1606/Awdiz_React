788bf80d77bf5e56e2a44b95692e9d22a0c73d6c
Sahiljoshi1606 <joshiom7387@gmail.com>
2025-08-06 23:47:22 +0530

    update

--- Day - 2/src/pages/01-08/UseParams.jsx
+++ Day - 2/src/pages/01-08/UseParams.jsx
@@ -3,17 +3,25 @@ import { useState } from 'react';
 
 export default function UseParams() {
     const [tshirt , setTshirt] = useState([{
-        id : 1 , name : "T-tshirt 1", price : 1000, image : "https://example.com/tshirt1.jpg"
+        id : 1 , name : "T-tshirt 1", price : 1000, image : "https://tse4.mm.bing.net/th/id/OIP.RHzRocDQ-VPOif5AmSbAeQHaKl?cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3"
     }
     , {
-        id : 2 , name : "T-tshirt 2", price : 1200, image : "https://example.com/tshirt2.jpg"
+        id : 2 , name : "T-tshirt 2", price : 1200, image : "https://tse4.mm.bing.net/th/id/OIP.RHzRocDQ-VPOif5AmSbAeQHaKl?cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3"
     }
     , {
-        id : 3 , name : "T-tshirt 3", price : 1500, image : "https://example.com/tshirt3.jpg"
+        id : 3 , name : "T-tshirt 3", price : 1500, image : "https://tse4.mm.bing.net/th/id/OIP.RHzRocDQ-VPOif5AmSbAeQHaKl?cb=thfc1&rs=1&pid=ImgDetMain&o=7&rm=3"
     }]);
     console.log(tshirt, "tshirt");
   return (
-
+    <div  style={{ display: 'flex', flexWrap: 'wrap', gap: '16px',  height : `10px` , width : `100%` }} className='tshirt-list'>
+      {tshirt.map((item) => (
+        <div key={item.id} className='tshirt'>
+          <h2>{item.name}</h2>
+          <p>Price: {item.price}</p>
+          <img src={item.image} alt={item.name} />
+        </div>
+      ))}
+    </div>
   )
 }
 
